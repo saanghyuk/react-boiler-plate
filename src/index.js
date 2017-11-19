7797d7682dd39ff4a85fa7c81cc50fc71dca558f
Sanghyuk <saanghyuk@gmail.com>
2017-11-19 18:14:34 +0900

    Add Router Setting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {Router, Route} from 'react-router';
+import createHistory from 'history/createBrowserHistory'
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import App from './components/app';
@@ -9,9 +12,14 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+export const history = createHistory();
+
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <App />
+        <Router history={history}>
+            <Route path="/" component={App}>
+            </Route>
+        </Router>
     </Provider>
     , document.querySelector('.container'));
 
